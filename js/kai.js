924d581e7c56295892e2ad89a4e25826c0602862
projectpacha <98872041+projectpacha@users.noreply.github.com>
2023-09-17 23:37:16 +0530

    Update kai.js

--- js/kai.js
+++ js/kai.js
@@ -1,4 +1,4 @@
-import { inputMappings, outputMappings, punctuationRules, featureRules } from './js/maps.js';
+import { inputMappings, outputMappings, punctuationRules, featureRules } from 'maps.js';
 //
 
 const supportedScripts = [
