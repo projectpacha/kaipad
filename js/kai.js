b48c2f1a0da48b70712bc379cb3bc31415aadcf8
projectpacha <98872041+projectpacha@users.noreply.github.com>
2023-09-18 08:26:54 +0530

    Update kai.js fiex

--- js/kai.js
+++ js/kai.js
@@ -1,4 +1,4 @@
-import { inputMappings, outputMappings, punctuationRules, featureRules } from 'maps.js';
+import { inputMappings, outputMappings, punctuationRules, featureRules } from './maps.js';
 //
 console.log("mnt");
 const supportedScripts = [
